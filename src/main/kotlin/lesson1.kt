b56db1425fe97b072976649fe0dce40c7f3fd536
Filin3 <3.14@belomor.su>
2022-12-24 18:50:58 +0500

    feature: Add task #1

---
+++ src/main/kotlin/lesson1.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val name = "Филипп"
+    val secondName = "Громов"
+    val profession = "Наладчик технологического оборудования"
+
+    println("$name $secondName $profession")
+}
