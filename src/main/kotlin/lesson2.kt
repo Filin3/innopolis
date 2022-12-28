7e1e37fab2aa15ff2a736e03dad73c1f7b729629
Filin3 <3.14@belomor.su>
2022-12-29 00:14:20 +0500

    feat: Add task #2

--- src/main/kotlin/lesson2.kt
+++ src/main/kotlin/lesson2.kt
@@ -1,5 +1,9 @@
+import kotlin.NumberFormatException
+import kotlin.math.abs
+
 fun main() {
     task1()
+    task2()
 }
 
 fun task1() {
@@ -13,5 +17,25 @@ fun task1() {
     }
 
     println(result)
+}
+
+fun task2() {
+    var sum:Int = 0
+    val number:Int
+    print("Write a number: ")
+
+    try {
+        number = readln().toInt()
+    } catch (e: NumberFormatException) {
+        return println("Only an integer is available for input")
+    }
+
+    for (i in 0..abs(number)) {
+        sum += i
+    }
+
+    if (number < 0) sum = -sum
+
+    println("Sum from 0 to $number is $sum")
 }
 
