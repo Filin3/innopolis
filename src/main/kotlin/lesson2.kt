90bf5ba040cc0050fa2103f6c6753f3be1fa47ad
Filin3 <3.14@belomor.su>
2022-12-29 00:15:52 +0500

    feat: Add task #5

--- src/main/kotlin/lesson2.kt
+++ src/main/kotlin/lesson2.kt
@@ -1,3 +1,4 @@
+import java.util.Calendar
 import kotlin.NumberFormatException
 import kotlin.math.abs
 
@@ -6,6 +7,7 @@ fun main() {
     task2()
     task3()
     task4()
+    task5()
 }
 
 fun task1() {
@@ -101,3 +103,84 @@ fun task4() {
     }
 }
 
+fun task5() {
+    val menu:String = "1. Ввести имя\n" +
+            "2. Ввести фамилию\n" +
+            "3. Ввести год рождения\n" +
+            "4. Вывести информацию\n" +
+            "0. Выход\n" +
+            "Введите цифру из меню: "
+
+    var name:String? = null
+    var secondName:String? = null
+    var yearOfBirthDay:Int? = null
+
+    fun printUserAbout() {
+        if (name != null && secondName != null && yearOfBirthDay != null) {
+            println("Имя: $name\n" +
+                    "Фамилия: $secondName\n" +
+                    "Год рождения: $yearOfBirthDay")
+        } else {
+            println("Вы забыли указать одно или более из следующих значений: имя, фамилия, год рождения")
+        }
+    }
+
+    fun setName() {
+        print("Введите имя: ")
+        val potentialName = readln()
+        if (strContainsOnlyLetters(potentialName)) {
+            name = potentialName
+        } else {
+            println("Имя не было задано так как введено некорректное значение")
+        }
+    }
+
+    fun setSecondName() {
+        print("Введите фамилию: ")
+        val potentialSecondName = readln()
+        if (strContainsOnlyLetters(potentialSecondName)) {
+            secondName = potentialSecondName
+        } else {
+            println("Фамилия не была задана так как введено некорректное значение")
+        }
+    }
+
+    fun setYearOfBirthDay() {
+        try {
+            print("Введите год рождения: ")
+            val potentialYear = readln().toInt()
+            if (potentialYear >= 1900 && potentialYear <= Calendar.getInstance().get(Calendar.YEAR)) {
+                yearOfBirthDay = potentialYear
+            } else {
+                println("Год рождения не был задан так как введено некорректное значение")
+            }
+        } catch (e: NumberFormatException) {
+            println("Для ввода доступны только целые числа")
+        }
+    }
+
+    while (true) {
+        val choice:Int
+        print(menu)
+        try {
+            choice = readln().toInt()
+            when (choice) {
+                1 -> setName()
+                2 -> setSecondName()
+                3 -> setYearOfBirthDay()
+                4 -> printUserAbout()
+                0 -> break
+            }
+        } catch (e: NumberFormatException) {
+            println("Для ввода доступны только целые числа")
+        }
+    }
+}
+
+fun strContainsOnlyLetters(str: String):Boolean {
+    val pattern = Regex("[a-zA-Zа-яА-Я]")
+    val matches = pattern.findAll(str)
+    val result = matches.map { it.value }.joinToString("")
+
+    return str == result
+}
